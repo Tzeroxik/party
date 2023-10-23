575959b851cabbdb2071fac9a31ab83613718e0c
Adriano Anastácio <adriano.anastacio@inm.pt>
2023-10-23 17:14:44 +0100

    added base structure

--- party-web-api/src/main/kotlin/com/shroomander/party/web/api/configuration/PartyConfiguration.java
+++ party-web-api/src/main/kotlin/com/shroomander/party/web/api/configuration/PartyConfiguration.java
@@ -1,8 +1,12 @@
 package com.shroomander.party.web.api.configuration;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@ComponentScan("com.shroomander.party.configuration")
+@EnableJpaRepositories(basePackages = "com.shroomander.party.persistence")
 public class PartyConfiguration {
 
 }
--- party-web-api/src/main/kotlin/com/shroomander/party/web/api/controller/user/UserController.kt
+++ party-web-api/src/main/kotlin/com/shroomander/party/web/api/controller/user/UserController.kt
@@ -1,6 +1,9 @@
 package com.shroomander.party.web.api.controller.user
 
 import com.shroomander.party.web.api.view.user.User
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,8 +12,9 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/user")
 class UserController {
 
-    @GetMapping
-    fun user() : List<User> {
-        return listOf(User(1L, "Adriano"))
+    @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun getUsers() : ResponseEntity<List<User>> {
+        val users = listOf(User(1L, "Adriano"))
+        return ResponseEntity(users, HttpStatus.OK)
     }
 }
